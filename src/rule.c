6b05f4be1c95082693ee2dcbe5ec74898691e299
Pawel Boening <Pawel.boening@web.de>
2015-02-21 23:52:07 +0100

    prototype for rule parser started

--- src/rule.c
+++ src/rule.c
@@ -6,7 +6,30 @@
 
 struct rule
 parse_rule(char *rawRule, int lineNr){
+	int i,j;
 	struct rule newRule;
+	int tokenCount = 0;
+	char token[15][RULESIZE];
 	//missing stuff
+	for(i = 0; i < 15 ; i++){
+		for(j = 0; j < RULESIZE-1; i++){
+			token[i][j] = *rawRule;
+			rawRule++;
+		}
+		rawRule++;
+		tokenCount++;
+	}
+
+	switch(tokenCount){
+	case 5: break;
+	case 6: break;
+	case 7: break;
+	case 8: break;
+	case 9: break;
+	case 10: break;
+	case 11: break;
+	case 12: break;
+	}
+
 	return newRule;
 }
