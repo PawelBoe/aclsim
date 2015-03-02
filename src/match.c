1d5fe6ef7cba2f04ca43381e1dbdba78374a07d4
Pawel Boening <Pawel.boening@web.de>
2015-03-02 17:34:05 +0100

    match checker finished

--- src/match.c
+++ src/match.c
@@ -6,20 +6,37 @@
 
 struct match
 check_match(struct vector *vector, struct rule *rule){
-	int i;
 	struct match newMatch;
 	newMatch.vectorNr = vector->number;
 	newMatch.ruleNr = rule->number;
-	//missing stuff:
-	//compare address range and port range
-	//set appropriate state
+
 	if (vector->protocol != rule->protocol){
 		newMatch.state = -1;
 		return newMatch;
 	}
-	for(i = 0; i < 4; i++){
-		
+
+	if(!(vector->srcIp.byte[0] >= rule->srcIp_start.byte[0]) ||
+	   !(vector->srcIp.byte[0] <= rule->srcIp_end.byte[0])){
+		newMatch.state = -1;
+		return newMatch;
 	}
+	if(!(vector->srcIp.byte[1] >= rule->srcIp_start.byte[1]) ||
+	   !(vector->srcIp.byte[1] <= rule->srcIp_end.byte[1])){
+		newMatch.state = -1;
+		return newMatch;
+	}
+	if(!(vector->srcIp.byte[2] >= rule->srcIp_start.byte[2]) ||
+	   !(vector->srcIp.byte[2] <= rule->srcIp_end.byte[2])){
+		newMatch.state = -1;
+		return newMatch;
+	}
+	if(!(vector->srcIp.byte[3] >= rule->srcIp_start.byte[3]) ||
+	   !(vector->srcIp.byte[3] <= rule->srcIp_end.byte[3])){
+		newMatch.state = -1;
+		return newMatch;
+	}
+
+	newMatch.state = rule->action;
 	return newMatch;
 }
 
