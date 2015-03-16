afa2827aeeb9e6ca9eaf6239c580db22708ef854
Pawel Boening <pawel.boening@student.hpi.uni-potsdam.de>
2015-03-16 19:03:20 +0100

    prepared for parse_rule implementation

--- src/rule.c
+++ src/rule.c
@@ -1,4 +1,6 @@
+#include <string.h>
 #include "rule.h"
+#include "ip.h"
 
 
 static int
@@ -7,38 +9,51 @@ valid_rule(char *rawRule){
     return 0;
 }
 
+static int
+parse_action(char *rawAction){
+    //missing stuff
+    return 0;
+}
+
+static int
+parse_address(char *tok1[], union ipAdr addr, union ipAdr wild, unsigned int *charsExtracted){
+
+    return 0;
+}
+
 error_t
 parse_rule(struct rule *newRule, char *rawRule, int lineNr){
     //missing validity checking
     valid_rule(rawRule);
 
-    int i,j;
-    int tokenCount = 0;
-    char token[15][RULESIZE];
-    //missing stuff
-    //count tokens, determine rule format
-    for(i = 0; i < 15 ; i++){
-        for(j = 0; j < RULESIZE-1; i++){
-            token[i][j] = *rawRule;
-            rawRule++;
-        }
-        rawRule++;
-        tokenCount++;
-    }
-    newRule->number = token[1][1];
+    int i;
+    char *token[12];
+    char buff[RULESIZE];
+    strncpy(buff, rawRule, RULESIZE);
+    buff[RULESIZE-1] = '\0';
 
-    switch(tokenCount){
-        case 4: break;
-        case 5: break;
-        case 6: break;
-        case 7: break;
-        case 8: break;
-        case 9: break;
-        case 10: break;
-        case 11: break;
-        case 12: break;
-        default: break;
+    union ipAdr addr;
+    union ipAdr wild;
+    unsigned int *tokensExtracted = 0;
+
+    newRule->number = lineNr;
+
+    token[0] = strtok(buff, " "); //action
+    newRule->action = parse_action(token[0]);
+    tokensExtracted++;
+
+    token[1] = strtok(NULL, " "); //protocol
+    newRule->protocol = parse_protocol(token[1]);
+    tokensExtracted++;
+
+    for (i = 2; i < 12; ++i){
+        token[i] = strtok(NULL, " ");
     }
+
+    parse_address(token, addr, wild, tokensExtracted);
+    //missing stuff
+
+
     return SUCCESS;
 }
 
