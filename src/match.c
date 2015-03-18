7bc6ad90dce3081b929938f39e5a51b9a0ca6328
Pawel Boening <pawel.boening@student.hpi.uni-potsdam.de>
2015-03-18 08:58:36 +0100

    removed bugs, added match functions

--- src/match.c
+++ src/match.c
@@ -12,9 +12,10 @@ check_match(struct match *newMatch, struct vector *vector, struct rule *rule){
     if (rule->action == AC_REMARK){
         newMatch->state = ST_NOMATCH;
     }
-    else if (vector->protocol != rule->protocol){ //IP as generic accept
+    else if (vector->protocol != rule->protocol && rule->protocol != PROTO_IP){
         newMatch->state = ST_NOMATCH;
     }
+
     else if(!(vector->srcIp.byte[0] >= rule->srcIpStart.byte[0]) ||
             !(vector->srcIp.byte[0] <= rule->srcIpEnd.byte[0])){
                 newMatch->state = ST_NOMATCH;
@@ -31,7 +32,33 @@ check_match(struct match *newMatch, struct vector *vector, struct rule *rule){
             !(vector->srcIp.byte[3] <= rule->srcIpEnd.byte[3])){
                 newMatch->state = ST_NOMATCH;
     }
-    //ports missing
+
+    else if(!(vector->dstIp.byte[0] >= rule->dstIpStart.byte[0]) ||
+            !(vector->dstIp.byte[0] <= rule->dstIpEnd.byte[0])){
+                newMatch->state = ST_NOMATCH;
+    }
+    else if(!(vector->dstIp.byte[1] >= rule->dstIpStart.byte[1]) ||
+            !(vector->dstIp.byte[1] <= rule->dstIpEnd.byte[1])){
+                newMatch->state = ST_NOMATCH;
+    }
+    else if(!(vector->dstIp.byte[2] >= rule->dstIpStart.byte[2]) ||
+            !(vector->dstIp.byte[2] <= rule->dstIpEnd.byte[2])){
+                newMatch->state = ST_NOMATCH;
+    }
+    else if(!(vector->dstIp.byte[3] >= rule->dstIpStart.byte[3]) ||
+            !(vector->dstIp.byte[3] <= rule->dstIpEnd.byte[3])){
+                newMatch->state = ST_NOMATCH;
+    }
+
+    else if(!(vector->srcPrt.value >= rule->srcPrtStart.value) ||
+            !(vector->srcPrt.value <= rule->srcPrtEnd.value)){
+                newMatch->state = ST_NOMATCH;
+    }
+    else if(!(vector->dstPrt.value >= rule->dstPrtStart.value) ||
+            !(vector->dstPrt.value <= rule->dstPrtEnd.value)){
+                newMatch->state = ST_NOMATCH;
+    }
+
     else{
         newMatch->state = rule->action;
     }
